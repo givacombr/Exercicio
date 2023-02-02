07d06a0eccb5e31ff389de7a66ca66bf11253287
Giv@s <101278555+givacombr@users.noreply.github.com>
2023-02-02 19:29:19 -0300

    atualizando

---
+++ Meus Arquivos/De0a100/De0a100/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace De0a100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i=0; i<=100; i=i+2)
+            {
+                Console.WriteLine(\n i);
+            }
+        }
+    }
+}
