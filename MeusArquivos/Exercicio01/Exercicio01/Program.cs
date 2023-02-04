ea2eb0d5daa1b41c2b63ad9a7078f7a1edf2f968
Giv@s <101278555+givacombr@users.noreply.github.com>
2023-02-04 18:12:59 -0300

    atualizado

--- MeusArquivos/Exercicio01/Exercicio01/Program.cs
+++ MeusArquivos/Exercicio01/Exercicio01/Program.cs
@@ -11,11 +11,11 @@
             Console.WriteLine("Digite outro valor: ");
             n2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("A soma dos números digitados são: " + (n1 + n2));
-            Console.WriteLine("A subtração dos números digitados são: " + (n1 - n2));
-            Console.WriteLine("A multiplicação dos números digitados são: " + (n1 * n2));
-            Console.WriteLine("A divisão dos números digitados são: " + (n1 / n2));
-            Console.WriteLine("O resto dos números digitados são: " + (n1 % n2));
+            Console.WriteLine("A soma dos números digitados de " + n1 + " mais " + n2 + " são: " + (n1 + n2));
+            Console.WriteLine("A subtração dos números digitados de " + n1 + " menos " + n2 + " são: " + (n1 - n2));
+            Console.WriteLine("A multiplicação dos números digitados de " + n1 + " vezes " + n2 + " são: " + (n1 * n2));
+            Console.WriteLine("A divisão dos números digitados de " + n1 + " dividido " + n2 + " são: " + (n1 / n2));
+            Console.WriteLine("O resto de uma divisão dos números digitados de " + n1 + " e " + n2 + " são: " + (n1 % n2));
 
         }
     }
---
+++ MeusArquivos/Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float x1, x2;
+            Console.WriteLine("Digite o valor do seu salário: R$ ");
+            x1
+        }
+    }
+}
