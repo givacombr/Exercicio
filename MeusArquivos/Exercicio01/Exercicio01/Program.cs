910c4ade4f6f61f7a9829024c42a5115a3303d0d
Giv@s <101278555+givacombr@users.noreply.github.com>
2023-02-04 17:40:40 -0300

    Ex01

---
+++ MeusArquivos/Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            
+            Console.WriteLine("Digite um valor: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite outro valor: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("A soma dos números digitados são: " + (n1 + n2));
+            Console.WriteLine("A subtração dos números digitados são: " + (n1 - n2));
+            Console.WriteLine("A multiplicação dos números digitados são: " + (n1 * n2));
+            Console.WriteLine("A divisão dos números digitados são: " + (n1 / n2));
+            Console.WriteLine("O resto dos números digitados são: " + (n1 % n2));
+
+        }
+    }
+}
