c5e37abf508d077909b347864d64eabab01325ca
Giv@s <101278555+givacombr@users.noreply.github.com>
2023-02-04 20:24:31 -0300

    ainda nada

---
+++ MeusArquivos/Zeroa100/Zeroa100/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Zeroa100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 0; i <= 100; i+=2)//i=i+2
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
